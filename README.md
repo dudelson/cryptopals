bd61efdb4e107d59935b24163968ac11d16cf3bc
David Udelson <dru5@cornell.edu>
2016-06-21 09:36:45 -0400

    Updated readme and added documentation to 1st challenge source file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cryptopals
-Solutions to the cryptopals crypto challenges
+Solutions to the [cryptopals crypto challenges](http://cryptopals.com/). I am 
+also using this project to teach myself [rust](https://www.rust-lang.org/), so
+hopefully as the challenges progress, the code quality will become less awful :P.
--- src/set1/challenge1-hexTo64.rs
+++ src/set1/challenge1-hexTo64.rs
@@ -1,6 +1,3 @@
-/* TODO:
- *   - validate input strings as hex
- */
 #![allow(non_snake_case)]
 
 use std::env;
@@ -8,7 +5,10 @@ use std::process;
 use std::char;
 use std::u32;
 
-/*
+/// given a u8, returns a string representing the binary form of that u8
+/// I'm not using this currently, but I'm keeping it around in case I need it
+/// later.
+#[allow(dead_code)]
 fn binString(x: &u8) -> String {
     let mut s: String = String::from("");
     for b in 0..8 {
@@ -19,17 +19,25 @@ fn binString(x: &u8) -> String {
     }
     s
 }
-*/
 
+/// given a string representing a hex value, returns a string representing the
+/// equivalent base64 value
 fn hexToBase64(hexStr: &str) -> String {
     let len = hexStr.len();
     let mut group = 0;
     let mut s = "".to_string();
-    while group*3 < len {
-        let hexValue: u32 = u32::from_str_radix(&hexStr[group*3 .. (group+1)*3], 16).unwrap();
-        let b64val1: u32 = (hexValue & (63 << 6)) >> 6;
-        let b64val2: u32 = hexValue & 63;
-        let v = vec![b64val1, b64val2];
+    // Each character in the input (hex) string represents 4 bits, and each
+    // character in the output (base64) string represents 6 bits. So I take the
+    // input characters in groups of 6 (6 chars * 4 bits/char = 24 bits, which
+    // fits nicely into a u32), and convert them into 4 base64 chars.
+    while group*6 < len {
+        let hexValue: u32 = u32::from_str_radix(&hexStr[group*6 .. (group+1)*6], 16).unwrap();
+        let v = vec![
+            (hexValue & (63 << 18)) >> 18,
+            (hexValue & (63 << 12)) >> 12,
+            (hexValue & (63 << 6)) >> 6,
+            hexValue & 63
+        ];
         for b64val in v {
             match b64val {
                 62          => s.push('+'),
